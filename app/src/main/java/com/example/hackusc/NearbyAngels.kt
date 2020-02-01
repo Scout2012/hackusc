1a6a8af522a2f6516017cbc5733c3e4d54a6474c
gramos <gramos@bu.edu>
2020-02-01 05:23:17 -0800

    angie

--- app/src/main/java/com/example/hackusc/NearbyAngels.kt
+++ app/src/main/java/com/example/hackusc/NearbyAngels.kt
@@ -3,10 +3,11 @@ package com.example.hackusc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
 class NearbyAngels : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nearby_angels_activity)
+
+
     }
 }
