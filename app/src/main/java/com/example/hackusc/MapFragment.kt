d3831a19ff3cf1c2db599654307b6085ef85456d
gramos <gramos@bu.edu>
2020-02-01 16:53:56 -0800

    fixed warnings

--- app/src/main/java/com/example/hackusc/MapFragment.kt
+++ app/src/main/java/com/example/hackusc/MapFragment.kt
@@ -99,18 +99,10 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+//        val sydney = LatLng(-34.0, 151.0)
 //        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 //        mMap.animateCamera(CameraUpdateFactory.zoomTo(mMap.maxZoomLevel))
-
-        if(!mMap.isMyLocationEnabled){
-            mMap.setMyLocationEnabled(true)
-        }
-
-        if(mMap != null) {
-            mMap.setOnMyLocationChangeListener {  }
-        }
     }
 
 
